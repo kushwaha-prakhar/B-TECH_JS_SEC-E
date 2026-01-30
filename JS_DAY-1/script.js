d7eb7d6bc9c6190bbf4cc96634a10e5f8b4f00a3
kushwaha-prakhar <kushwahaprakhar20@gmail.com>
2026-01-30 11:54:56 +0530

    first commit

---
+++ JS_DAY-1/script.js
@@ -0,0 +1,36 @@
+// Types of function
+
+// 1. regular function
+
+// function sample(){
+//     return "sample";
+// }
+// console.log(sample());
+
+// 2. arrow function
+// It doesn't need return statement and it follows asynchronous operation
+
+// const sample=(a,b)=>a+b;
+// console.log(sample(5,10))
+
+// const sample=(a,b)=>{
+//     return a+b;
+// }
+// console.log(sample(5,10))
+
+
+// Asynchronous function : we can move to another task before the previous one finishes its execution. 
+
+// 3. callback function
+// if we pass a function as an argument inside another function
+// callback=demo()
+// function sample(callback){
+//     console.log("sample");
+//     callback();
+// }
+// function demo(){
+//     console.log("demo");
+// }
+// sample(demo);
+
+
