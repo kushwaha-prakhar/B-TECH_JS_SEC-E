d1322d8af4f61f42c6ee7dd084bc17183c9c4f8f
kushwaha-prakhar <kushwahaprakhar20@gmail.com>
2026-02-02 13:37:57 +0530

    secondcommit

---
+++ JS-DAY-8/script.js
@@ -0,0 +1,20 @@
+// task 1
+const profile = document.getElementById("profile");
+profile.style.backgroundColor= "cyan";
+profile.style.padding = "15px";
+profile.style.textAlign="center";
+
+
+// task 2
+const highlight = document.getElementsByClassName("important");
+console.log(highlight.length)
+
+for(let i=0 ; i<highlight.length;i++){
+    highlight[i].style.color= "blue";
+    highlight[i].style.fontSize= "30px";
+    
+}
+
+// task 3
+// make evne p blue
+// make odd p blue
